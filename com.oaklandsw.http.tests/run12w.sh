b2b86d63a816cdbabb384cc51d77a7afc0a66973
unknown <unknown@localhost>
2005-12-18 02:46:21 +0000

    [svn r833] fixed test execution files for windows

---
+++ com.oaklandsw.http.tests/run12w.sh
@@ -0,0 +1,1 @@
+java  -Dlog4j.configuration=log4j.properties -Doaklandsw.localhost=repoman -Djava.net.preferIPv4Stack=true -cp "..\\com.oaklandsw.http.tests\\httptests.jar\;..\\com.oaklandsw.http.tests.jars\\jsse103.jar\;..\\com.oaklandsw.http.tests.jars\\jce-jdk12-115.jar\;..\\com.oaklandsw.http.tests.jars\\junit.jar\;..\\com.oaklandsw.http\\http.jar" com.oaklandsw.http.TestAll
