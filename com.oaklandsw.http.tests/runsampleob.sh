696d7ca6d667a87d8ddb41dbd13bc3e9a3f58381
unknown <unknown@localhost>
2006-07-09 07:45:25 +0000

    [svn r1601] Update HTTP test scripts to remove JCE library from classpath

--- com.oaklandsw.http.tests/runsampleob.sh
+++ com.oaklandsw.http.tests/runsampleob.sh
@@ -1,2 +1,2 @@
-javac -classpath .:../com.oaklandsw.http/httpOb.jar:../com.oaklandsw.http.tests.jars/jce-jdk12-115.jar HttpGetSample.java
-java -Djava.net.preferIPv4Stack=true -cp .:../com.oaklandsw.http/httpOb.jar:../com.oaklandsw.http.tests.jars/jce-jdk12-115.jar HttpGetSample $*
+javac -classpath .:../com.oaklandsw.http/httpOb.jar HttpGetSample.java
+java -Djava.net.preferIPv4Stack=true -cp .:../com.oaklandsw.http/httpOb.jar HttpGetSample $*
