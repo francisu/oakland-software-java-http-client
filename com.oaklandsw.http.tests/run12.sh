529eb676b2e58cd60bf04a4fa0495978d7c2405c
unknown <unknown@localhost>
2006-08-16 18:04:01 +0000

    [svn r1692] Remove obsolete scripts

--- com.oaklandsw.http.tests/run12.sh
+++
@@ -1,1 +0,0 @@
-java -verbose -Dlog4j.configuration=../com.oaklandsw.build/src/logprops/ljp.none  -Doaklandsw.localhost=repoman -Djava.net.preferIPv4Stack=true -cp ../com.oaklandsw.http.tests/httptests.jar:../com.oaklandsw.http.tests.jars/jsse103.jar:../com.oaklandsw.http.tests.jars/junit.jar:../com.oaklandsw.http/httpNonObf.jar com.oaklandsw.http.TestAll
--- com.oaklandsw.http.tests/run14w.sh
+++
@@ -1,1 +0,0 @@
-java  -Djava.util.logging.config.file=../com.oaklandsw.build/src/logprops/ljp.none  -Doaklandsw.localhost=repoman -Djava.net.preferIPv4Stack=true -cp "..\\com.oaklandsw.http.tests\\httptests.jar\;..\\com.oaklandsw.http.tests.jars\\junit.jar\;..\\com.oaklandsw.http\\httpNonObf.jar" com.oaklandsw.http.TestAll
